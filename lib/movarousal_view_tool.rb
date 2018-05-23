cfd6e5a339b7ffe737933f6e374ca1b9b72b38f6
ediliu <a30745m@gmail.com>
2018-05-23 20:31:47 +0800

    Movarousal tool iniial commit

---
+++ lib/movarousal_view_tool.rb
@@ -0,0 +1,6 @@
+require "movarousal_view_tool/version"
+require "movarousal_view_tool/renderer"
+
+module MovarousalViewTool
+  # Your code goes here...
+end
---
+++ lib/movarousal_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MovarousalViewTool
+  VERSION = "0.1.0"
+end
